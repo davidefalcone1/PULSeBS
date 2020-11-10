9ad04752fe6b37ff63eb3b410cd21dac3cd4d210
Iacopo Giuseppe Foderini s279031 <s279031@studenti.polito.it>
2020-11-10 10:25:22 +0100

    set up backend, added login backend

--- server/dao/Counter.js
+++
@@ -1,7 +0,0 @@
-class Counter {
-    constructor(COUNTER_ID) {
-      if (COUNTER_ID) this.COUNTER_ID = COUNTER_ID;
-    }
-  }
-  
-  module.exports = Counter;
--- server/dao/Service.js
+++
@@ -1,9 +0,0 @@
-class Service {
-  constructor(SERVICE_ID, NAME, SERVICE_TIME) {
-    if (SERVICE_ID) this.SERVICE_ID = SERVICE_ID;
-    this.NAME = NAME;
-    this.SERVICE_TIME = SERVICE_TIME;
-  }
-}
-
-module.exports = Service;
