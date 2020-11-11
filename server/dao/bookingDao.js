768365eb2f85cfbc761611fc3929369410c4149f
Iacopo Giuseppe Foderini s279031 <s279031@studenti.polito.it>
2020-11-11 17:03:29 +0100

    [PUL-39] added new dao file

---
+++ server/dao/bookingDao.js
@@ -0,0 +1,11 @@
+'use strict'
+
+exports.deleteBooking = (bookingID) => {
+    return new Promise ((resolve, reject) => {
+        if(!bookingID){
+            reject('Missing data');
+        }
+
+       // const sql = ``
+    });
+}
