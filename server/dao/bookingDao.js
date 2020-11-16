f7ed8673f54f544b75082af63501e7f0193e1bbf
Iacopo Giuseppe Foderini s279031 <s279031@studenti.polito.it>
2020-11-16 12:07:49 +0100

    [PUL-5] fixed bug

--- server/dao/bookingDao.js
+++ server/dao/bookingDao.js
@@ -2,21 +2,43 @@
 const db = require('../db');
 
 exports.deleteBooking = (bookingID) => {
-    return new Promise ((resolve, reject) => {
-        if(!bookingID){
+    return new Promise((resolve, reject) => {
+        if (!bookingID) {
             reject('Missing data');
         }
 
-        const sql = `UPDATE Booking 
-        SET BookStatus = 2 
-        WHERE BookID = ?`;
+        let sql = `UPDATE Booking 
+                   SET BookStatus = 2 
+                   WHERE BookID = ?`;
 
-        db.run(sql, bookingID, (err) => {
-            if(err){
+        db.run(sql, [bookingID], (err) => {
+            if (err) {
                 reject(err);
             }
             else {
-                resolve(null);
+                sql = `SELECT CourseScheduleID 
+                       FROM Booking  
+                       WHERE BookID = ?`;
+
+                db.get(sql, [bookingID], (err, row) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    else {
+                        sql = `UPDATE CourseSchedule 
+                               SET OccupiedSeat = OccupiedSeat - 1
+                               WHERE CourseScheduleID = ?`;
+                        db.run(sql, [row.CourseScheduleID], (err) => {
+                            if (err) {
+                                reject(err);
+                            }
+                            else {
+                                resolve(null);
+                            }
+                        })
+                    }
+                });
+
             }
         });
     });
