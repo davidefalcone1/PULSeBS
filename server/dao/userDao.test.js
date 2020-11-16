8269840e64ab8e16ba16724bbb14fe58e7eef923
davidefalcone1 <davidefalcone1@gmail.com>
2020-11-16 22:03:27 +0100

    added mock db for testing

--- server/dao/userDao.test.js
+++ server/dao/userDao.test.js
@@ -1,7 +1,23 @@
+jest.setMock("../db", require("../db.mock"));
+const db = require("../db");
 const User = require('./User');
 const userDao = require('./userDao');
 
 describe("getUser", ()=>{
+    beforeEach(()=>{
+        const sql = "INSERT INTO User(UserID, FullName, UserName, Password, AccessLevel) VALUES('275330', 'John Doe', 'john@polito.it', '$2b$12$7iALJ38k/PBlAB7b8JDksu7v85z.tjnC9XfoMdUJd75bIId87Ip2S', 1)";
+        db.run((sql), function (err, rows){
+            if(err)
+                console.log(err);
+        });
+    });
+    afterEach(()=>{
+        const sql = "DELETE FROM User WHERE ID=1";
+        db.run((sql), function (err, rows){
+            if(err)
+                console.log(err);
+        });
+    });
     it("user exists", ()=>{
         expect.assertions(1);
         const expectedUser = new User("275330", "john@polito.it", "$2b$12$7iALJ38k/PBlAB7b8JDksu7v85z.tjnC9XfoMdUJd75bIId87Ip2S", 1);
@@ -16,6 +32,20 @@ describe("getUser", ()=>{
 });
 
 describe("checkPassword", ()=>{
+    beforeEach(()=>{
+        const sql = "INSERT INTO User(UserID, FullName, UserName, Password, AccessLevel) VALUES('275330', 'John Doe', 'john@polito.it', '$2b$12$7iALJ38k/PBlAB7b8JDksu7v85z.tjnC9XfoMdUJd75bIId87Ip2S', 1)";
+        db.run((sql), function (err, rows){
+            if(err)
+                console.log(err);
+        });
+    });
+    afterEach(()=>{
+        const sql = "DELETE FROM User WHERE ID=1";
+        db.run((sql), function (err, rows){
+            if(err)
+                console.log(err);
+        });
+    });
     test("password ok", ()=>{
         const user = new User("275330", "john@polito.it", "$2b$12$7iALJ38k/PBlAB7b8JDksu7v85z.tjnC9XfoMdUJd75bIId87Ip2S", 1);
         expect(userDao.checkPassword(user, "adminadmin")).toBeTruthy();
