3aaaede5a887d842157f728bce3c31a43d69f804
Iacopo Giuseppe Foderini s279031 <s279031@studenti.polito.it>
2020-11-23 00:11:54 +0100

    fixed minor bug

--- server/dailyMailer.js
+++ server/dailyMailer.js
@@ -4,23 +4,23 @@ const cron = require('node-cron');
 const emailDao = require('./dao/emailDao');
 const emailAPI = require('./emailAPI');
 
-exports.setDailyMail = () => {
-    cron.schedule('0 23 * * *', () => {
+exports.setDailyMail = async () => {
+    cron.schedule('00 23 * * *', async() => {
 
-        emailDao.getProfessorsToNotify()
-            .then((professors) => {
-                professors.forEach((professor) => {
-                    const {email, ...info} = professor;
-                    info.notificationType = 2;
-                    emailAPI.sendNotification(email, info)
-                    .then();
-                })
-                
-            })
-            .catch((error) => {
-                console.log('Automatic email sending failed, thecause is the following error:');
-                console.log(error);
-            })
+        try{
+            const professors = await emailDao.getProfessorsToNotify();
+            for(let i = 0; i < professors.length; i++){
+                const{email, ...info} = professors[i];
+                info.notificationType = 2;
+                const response = await emailAPI.sendNotification(email, info);
+                if(!response){
+                    console.log('Automatic email sending failed, thecause is the following error')
+                }
+            }
+        }
+        catch(error){
+            console.log('Automatic email sending failed, thecause is the following error:');
+            console.log(error);
     
     });
 
