2a95e73a54ffafcecff1e0e03be0a579738b4667
davidefalcone1 <davidefalcone1@gmail.com>
2021-01-03 17:22:31 +0100

    ConfigureLessonsPage testing

--- client/__tests__/ConfigureLessonsPage.test.js
+++ client/__tests__/ConfigureLessonsPage.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { AuthContext } from '../src/_services/AuthContext';
 import ConfigureLessonsPage from '../src/components/ConfigureLessonsPage';
-import { BrowserRouter } from 'react-router-dom';
 import LessonData from '../src/API/LessonData';
 import UserData from '../src/API/UserData';
 import CourseData from '../src/API/CourseData';
@@ -12,15 +9,31 @@ import moment from 'moment';
 Date.now = jest.fn(() => 1482363367071);//Avoid dynamic content on the page
 
 describe('ConfigureLessonsPage', ()=>{
-    test('Renders correctly', ()=>{
-        const component = renderer.create(
-            <BrowserRouter>
-                <AuthContext.Provider value={{user: new UserData(1, '12345', 'Davide Falcone', 'davide.falcone@polito.it')}}>
-                    <ConfigureLessonsPage classesList={[new ClassroomData(1, 'A1', 80)]} coursesList={[new CourseData('XY123', 'Software engineering 2', '654321', 2, 3, 5, 6, 7, 3, 5, 7)]} lessonsList={[new LessonData(1, 'XY123', moment('2020-09-08T14:30'), moment('2020-09-08T17:00'), 9, 90, false, true, 'A1', 100, 4, 33, 0)]} createNewLesson={()=>{}} editLesson={()=>{}} uploadFileLessons={()=>{}}/>
-                </AuthContext.Provider>
-            </BrowserRouter>
+    let component;
+    beforeAll(()=>{
+        component = mount(
+            <ConfigureLessonsPage classesList={[new ClassroomData(1, 'A1', 80)]} coursesList={[new CourseData('XY123', 'Software engineering 2', '654321', 2, 3, 5, 6, 7, 3, 5, 7)]} lessonsList={[new LessonData(1, 'XY123', moment('2020-09-08T14:30'), moment('2020-09-08T17:00'), 9, 90, false, true, 'A1', 100, 4, 33, 0)]} createNewLesson={()=>{}} editLesson={()=>{}} uploadFileLessons={()=>{}}/>,
+            {
+                context: {user: new UserData(1, '12345', 'Davide Falcone', 'davidefalcone@polito.it', true)}
+            }
         );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+    });
+    test('lesson modal', ()=>{
+        component.find('#activateModalOfLessons').at(0).simulate('click');
+        expect(component).toContainMatchingElement('#newCourseForm');
+        const form = component.find('#newCourseForm').at(0);
+        form.find('select').at(0).simulate('change', {target: {value: 'Software engineering 2'}});
+        form.find('select').at(1).simulate('change', {target: {value: 'Active'}});
+        form.find('select').at(2).simulate('change', {target: {value: 'Remote'}});
+        form.find('select').at(3).simulate('change', {target: {value: '1 - 120'}});
+        form.find('input').at(0).simulate('change', {target: {value: 'new-value'}});
+        form.find('input').at(1).simulate('change', {target: {value: 'new-value'}});
+        form.simulate('submit');
+    });
+    test('file lesson modal', ()=>{
+        component.find('#uploadFileOfLessons').at(0).simulate('click');
+        expect(component).toContainMatchingElement('#newLessonFormFile');
+        const form = component.find('#newLessonFormFile').at(0);
+        form.simulate('submit');
     });
 });
